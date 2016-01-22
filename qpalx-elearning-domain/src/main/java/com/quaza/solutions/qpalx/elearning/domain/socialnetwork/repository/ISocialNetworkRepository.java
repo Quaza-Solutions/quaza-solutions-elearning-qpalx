4679d6fd0054545d51c5a2a2835e33b799562635
manyce400 <fallon12>
2016-01-22 13:19:32 -0500

    Adding all Business domain objects for  QPalX Elearning domain module

--- qpalx-elearning-domain/src/main/java/com/quaza/solutions/qpalx/elearning/domain/socialnetwork/repository/ISocialNetworkRepository.java
+++ qpalx-elearning-domain/src/main/java/com/quaza/solutions/qpalx/elearning/domain/socialnetwork/repository/ISocialNetworkRepository.java
@@ -1,7 +1,17 @@
 package com.quaza.solutions.qpalx.elearning.domain.socialnetwork.repository;
 
+import com.quaza.solutions.qpalx.elearning.domain.socialnetwork.SocialNetwork;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
 /**
- * Created by manyce400 on 1/22/16.
+ * Repository object for SocialNetwork
+ *
+ * @author manyce400
  */
-public interface ISocialNetworkRepository {
+public interface ISocialNetworkRepository extends CrudRepository<SocialNetwork, Long> {
+
+
+    @Query("Select sNetwork From SocialNetwork sNetwork Where sNetwork.socialNetworkName = ?1")
+    public SocialNetwork findBySocialNetworkName(String socialNetworkName);
 }
