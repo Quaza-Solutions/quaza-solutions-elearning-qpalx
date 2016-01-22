2eebde9744c8c190e9845008a9f5cbe0702a55c6
manyce400 <fallon12>
2016-01-22 12:53:16 -0500

    Adding all Business domain objects for  QPalX Elearning domain module

---
+++ qpalx-elearning-domain/src/main/java/com/quaza/solutions/qpalx/elearning/domain/geographical/repository/IQPalXCountryRepository.java
@@ -0,0 +1,12 @@
+package com.quaza.solutions.qpalx.elearning.domain.geographical.repository;
+
+import com.quaza.solutions.qpalx.elearning.domain.geographical.QPalXCountry;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author manyce400
+ */
+public interface IQPalXCountryRepository extends CrudRepository<QPalXCountry, Long> {
+
+}
---
+++ qpalx-elearning-domain/src/main/java/com/quaza/solutions/qpalx/elearning/domain/geographical/repository/IQPalXMunicipalityRepository.java
@@ -0,0 +1,13 @@
+package com.quaza.solutions.qpalx.elearning.domain.geographical.repository;
+
+import com.quaza.solutions.qpalx.elearning.domain.geographical.QPalXMunicipality;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author manyce400
+ */
+public interface IQPalXMunicipalityRepository  extends CrudRepository<QPalXMunicipality, Long> {
+}
+
+
