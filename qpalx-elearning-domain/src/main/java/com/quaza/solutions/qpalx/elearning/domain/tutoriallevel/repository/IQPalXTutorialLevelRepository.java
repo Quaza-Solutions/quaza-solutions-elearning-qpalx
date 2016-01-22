9a493ce11c6bdaacb28def810fba5b273cd17cca
manyce400 <fallon12>
2016-01-22 13:02:35 -0500

    Adding all Business domain objects for  QPalX Elearning domain module

---
+++ qpalx-elearning-domain/src/main/java/com/quaza/solutions/qpalx/elearning/domain/tutoriallevel/repository/IQPalXTutorialLevelRepository.java
@@ -0,0 +1,12 @@
+package com.quaza.solutions.qpalx.elearning.domain.tutoriallevel.repository;
+
+import com.quaza.solutions.qpalx.elearning.domain.tutoriallevel.QPalXTutorialLevel;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * JPA Repository object for QPalXTutorialRepository
+ *
+ * @author manyce400
+ */
+public interface IQPalXTutorialLevelRepository extends CrudRepository<QPalXTutorialLevel, Long> {
+}
