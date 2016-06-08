0e87f6dde854b4c89fa68c3b12c8893a9acc8ba5
manyce400 <fallon12>
2016-06-07 22:15:42 -0400

    Integrating with new front end implementation by Maureen.

---
+++ qpalx-elearning-web/src/main/java/com/quaza/solutions/qpalx/elearning/web/signup/student/StudentSignUpController.java
@@ -0,0 +1,27 @@
+package com.quaza.solutions.qpalx.elearning.web.signup.student;
+
+import com.quaza.solutions.qpalx.elearning.web.signup.SignUpSelectionWebVO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author manyce400
+ */
+@Controller
+public class StudentSignUpController {
+
+
+
+    private static final org.slf4j.Logger LOGGER = org.slf4j.LoggerFactory.getLogger(StudentSignUpController.class);
+
+
+    @RequestMapping(value = "/select-signup-payment", method = RequestMethod.POST)
+    public String selectSignUpPaymentPage(final ModelMap modelMap, Model model) {
+        LOGGER.info("Processing student signup payment page");
+        model.addAttribute("SignUpSelectionWebVO", new SignUpSelectionWebVO());
+        return "student-signup/payment";
+    }
+}
