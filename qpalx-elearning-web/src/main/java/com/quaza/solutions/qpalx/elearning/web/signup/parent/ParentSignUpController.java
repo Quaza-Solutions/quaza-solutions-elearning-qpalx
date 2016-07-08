4d5cf5f0f2a0df0a564259781577264909340eca
manyce400 <fallon12>
2016-07-08 16:42:16 -0400

    version 1.0 functionality

--- qpalx-elearning-web/src/main/java/com/quaza/solutions/qpalx/elearning/web/signup/parent/ParentSignUpController.java
+++ qpalx-elearning-web/src/main/java/com/quaza/solutions/qpalx/elearning/web/signup/parent/ParentSignUpController.java
@@ -5,8 +5,6 @@ import com.quaza.solutions.qpalx.elearning.web.qpalxuser.QPalXWebUserVO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 /**
@@ -21,7 +19,7 @@ public class ParentSignUpController {
     private static final org.slf4j.Logger LOGGER = org.slf4j.LoggerFactory.getLogger(ParentSignUpController.class);
 
 
-    @RequestMapping(value = "/select-signup-payment", method = RequestMethod.POST)
+    //@RequestMapping(value = "/select-signup-payment", method = RequestMethod.POST)
     public String selectSignUpPaymentPage(Model model, @ModelAttribute("QPalXWebUserVO") QPalXWebUserVO qPalXWebUserVO) {
         LOGGER.info("Student signup payment requested with qPalXWebUserVO: {}", qPalXWebUserVO);
         return ContentRootE.Student_Signup.getContentRootPagePath("payment");
