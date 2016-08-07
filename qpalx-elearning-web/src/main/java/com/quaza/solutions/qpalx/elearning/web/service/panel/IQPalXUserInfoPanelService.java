74f302b3de86178b4e586c7b61866597da307545
manyce400 <fallon12>
2016-08-07 00:06:35 -0400

    Version 1.0 functionality.  Additional changes for Student navigation.

---
+++ qpalx-elearning-web/src/main/java/com/quaza/solutions/qpalx/elearning/web/service/panel/IQPalXUserInfoPanelService.java
@@ -0,0 +1,15 @@
+package com.quaza.solutions.qpalx.elearning.web.service.panel;
+
+import org.springframework.ui.Model;
+
+/**
+ * Service that creates all required information for display on User's information panel
+ *
+ * @author manyce400
+ */
+public interface IQPalXUserInfoPanelService {
+
+
+    public void addUserInfoPanelAttributes(Model model);
+
+}
