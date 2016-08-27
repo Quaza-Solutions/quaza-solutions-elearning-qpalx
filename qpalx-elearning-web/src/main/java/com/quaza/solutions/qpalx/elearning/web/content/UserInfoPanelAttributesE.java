902d08544b539e5425b82a425cdc4d1596c00b45
manyce400 <fallon12>
2016-08-26 21:28:14 -0400

    Version 1.0 functionality.

---
+++ qpalx-elearning-web/src/main/java/com/quaza/solutions/qpalx/elearning/web/content/UserInfoPanelAttributesE.java
@@ -0,0 +1,14 @@
+package com.quaza.solutions.qpalx.elearning.web.content;
+
+/**
+ * @author manyce400
+ */
+public enum  UserInfoPanelAttributesE {
+
+
+    LoggedInQPalXUser,
+
+    QPalXUserType;
+
+
+}
