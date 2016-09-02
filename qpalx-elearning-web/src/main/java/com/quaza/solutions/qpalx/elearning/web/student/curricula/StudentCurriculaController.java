809d41c8687c6cecf894e5c814662e8cf4490cba
manyce400 <fallon12>
2016-09-01 20:03:55 -0400

    Version 1.0 functionality:  Matching up student home pages with changes from admin.

--- qpalx-elearning-web/src/main/java/com/quaza/solutions/qpalx/elearning/web/student/curricula/StudentCurriculaController.java
+++ qpalx-elearning-web/src/main/java/com/quaza/solutions/qpalx/elearning/web/student/curricula/StudentCurriculaController.java
@@ -1,7 +1,9 @@
 package com.quaza.solutions.qpalx.elearning.web.student.curricula;
 
 import com.quaza.solutions.qpalx.elearning.domain.lms.curriculum.ELearningCourse;
+import com.quaza.solutions.qpalx.elearning.domain.lms.curriculum.ELearningCourseActivity;
 import com.quaza.solutions.qpalx.elearning.domain.lms.curriculum.ELearningCurriculum;
+import com.quaza.solutions.qpalx.elearning.service.lms.curriculum.IELearningCourseActivityService;
 import com.quaza.solutions.qpalx.elearning.service.lms.curriculum.IELearningCourseService;
 import com.quaza.solutions.qpalx.elearning.service.lms.curriculum.IELearningCurriculumService;
 import com.quaza.solutions.qpalx.elearning.web.content.ContentRootE;
@@ -39,6 +41,10 @@ public class StudentCurriculaController {
     private IELearningCourseService ieLearningCourseService;
 
     @Autowired
+    @Qualifier("quaza.solutions.qpalx.elearning.service.DefaultELearningCourseActivityService")
+    private IELearningCourseActivityService ieLearningCourseActivityService;
+
+    @Autowired
     @Qualifier("quaza.solutions.qpalx.elearning.web.QPalXUserInfoPanelService")
     private IQPalXUserInfoPanelService qPalXUserInfoPanelService;
 
@@ -61,10 +67,14 @@ public class StudentCurriculaController {
     public String displayQPalXCourseActivity(final Model model, @RequestParam("qCourseID") String qCourseID) {
         LOGGER.info("Retrieving all learning activities in qCourseID: {}", qCourseID);
 
+        Long id = NumberUtils.toLong(qCourseID);
+        ELearningCourse eLearningCourse = ieLearningCourseService.findByCourseID(id);
+
         // Add all attributes required for User information panel
         qPalXUserInfoPanelService.addUserInfoAttributes(model);
 
         addSelectedCourseInfoToResponse(model, qCourseID);
+        addSelectedCurriculumInfoToResponse(model, eLearningCourse.geteLearningCurriculum().getId().toString());
         return ContentRootE.Student_Home.getContentRootPagePath("course-activities");
     }
 
@@ -72,9 +82,21 @@ public class StudentCurriculaController {
     public String playQCourseActivity(final Model model, @RequestParam("courseActivityID") String courseActivityID) {
         LOGGER.info("Accessing Q Course acitivity with courseActivityID: {}", courseActivityID);
 
+        Long id = NumberUtils.toLong(courseActivityID);
+        ELearningCourseActivity eLearningCourseActivity = ieLearningCourseActivityService.findByID(id);
+        ELearningCourse eLearningCourse = eLearningCourseActivity.geteLearningCourse();
+        ELearningCurriculum eLearningCurriculum = eLearningCourse.geteLearningCurriculum();
+        String studentTutorialGradeID = eLearningCurriculum.getStudentTutorialGrade().getId().toString();
+        String curriculumType = eLearningCurriculum.getCurriculumType().toString();
+
         // Add all attributes required for User information panel
         qPalXUserInfoPanelService.addUserInfoAttributes(model);
-
+        addSelectedCurriculumInfoToResponse(model, eLearningCourseActivity.geteLearningCourse().geteLearningCurriculum().getId().toString());
+        model.addAttribute("SelectedELearningCurriculum", eLearningCurriculum);
+        model.addAttribute("SelectedELearningCourse", eLearningCourse);
+        model.addAttribute("SelectedELearningCourseActivity", eLearningCourseActivity);
+        model.addAttribute("SelectedELearningCourseActivityFile", eLearningCourseActivity.geteLearningMediaContent().getELearningMediaFile());
+        model.addAttribute("SelectedMediaContentType", eLearningCourseActivity.geteLearningMediaContent().getELearningMediaType());
         return ContentRootE.Student_Home.getContentRootPagePath("video-widget");
     }
 
