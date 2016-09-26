72fe99998e6e3682b3c5d7a9ddcec2d935151576
manyce400 <fallon12>
2016-09-26 06:20:25 -0400

    Version 1.0 functionality:  Adding support for Lessons, MicroLessons, MicroLessons activities

---
+++ qpalx-elearning-domain/src/main/java/com/quaza/solutions/qpalx/elearning/domain/lms/curriculum/repository/IQPalXEMicroLessonRepository.java
@@ -0,0 +1,13 @@
+package com.quaza.solutions.qpalx.elearning.domain.lms.curriculum.repository;
+
+import com.quaza.solutions.qpalx.elearning.domain.lms.curriculum.QPalXEMicroLesson;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * @author manyce400
+ */
+public interface IQPalXEMicroLessonRepository extends CrudRepository<QPalXEMicroLesson, Long> {
+
+
+
+}
