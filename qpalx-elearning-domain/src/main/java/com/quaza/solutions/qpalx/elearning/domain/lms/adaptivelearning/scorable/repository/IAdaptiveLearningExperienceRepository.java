f7c6327ffd25b9bb9d24cf97cd6b6d4d34401d1d
manyce400 <fallon12>
2016-10-12 23:09:31 -0400

    Version 1.0 functionality:  Adding scorable lesson activities functionality

--- qpalx-elearning-domain/src/main/java/com/quaza/solutions/qpalx/elearning/domain/lms/adaptivelearning/scorable/repository/IAdaptiveLearningExperienceRepository.java
+++ qpalx-elearning-domain/src/main/java/com/quaza/solutions/qpalx/elearning/domain/lms/adaptivelearning/scorable/repository/IAdaptiveLearningExperienceRepository.java
@@ -1,12 +1,7 @@
 package com.quaza.solutions.qpalx.elearning.domain.lms.adaptivelearning.scorable.repository;
 
 import com.quaza.solutions.qpalx.elearning.domain.lms.adaptivelearning.scorable.AdaptiveLearningExperience;
-import com.quaza.solutions.qpalx.elearning.domain.lms.curriculum.ELearningCurriculum;
-import com.quaza.solutions.qpalx.elearning.domain.qpalxuser.QPalXUser;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
 
 /**
  *
@@ -22,16 +17,14 @@ public interface IAdaptiveLearningExperienceRepository extends CrudRepository<Ad
      * @param eLearningCurriculum
      * @return List<AdaptiveLearningExperience>
      */
-    @Query(
-            "Select         adaptiveLearningExperience "+
-            "From           AdaptiveLearningExperience adaptiveLearningExperience "+
-            "INNER  JOIN    FETCH adaptiveLearningExperience.qpalxUser qpalxUser "+
-            "INNER  JOIN    FETCH adaptiveLearningExperience.eLearningCourseActivity as eLearningCourseActivity "+
-            "INNER  JOIN    FETCH eLearningCourseActivity.eLearningCourse as eLearningCourse "+
-            "INNER  JOIN    FETCH eLearningCourse.eLearningCurriculum as eLearningCurriculum "+
-            "Where          qpalxUser = ?1 "+
-            "And            eLearningCurriculum =?2"
-    )
-    public List<AdaptiveLearningExperience> findAllQPalxUserCurriculumLearningExperiences(final QPalXUser qPalXUser, ELearningCurriculum eLearningCurriculum);
+//    @Query(
+//            "Select         adaptiveLearningExperience "+
+//            "From           AdaptiveLearningExperience adaptiveLearningExperience "+
+//            "INNER  JOIN    FETCH adaptiveLearningExperience.qpalxUser qpalxUser "+
+//            "INNER  JOIN    FETCH eLearningCourse.eLearningCurriculum as eLearningCurriculum "+
+//            "Where          qpalxUser = ?1 "+
+//            "And            eLearningCurriculum =?2"
+//    )
+//    public List<AdaptiveLearningExperience> findAllQPalxUserCurriculumLearningExperiences(final QPalXUser qPalXUser, ELearningCurriculum eLearningCurriculum);
 
 }
