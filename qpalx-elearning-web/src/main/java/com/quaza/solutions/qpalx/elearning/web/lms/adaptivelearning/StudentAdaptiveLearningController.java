5f70752c2137326f3d1606adabd0414d998c21c8
manyce400 <fallon12>
2016-10-21 00:02:10 -0400

    Version 1.0 functionality:  Dynamic generation of user overview panel and tutorial level calendar panel.

--- qpalx-elearning-web/src/main/java/com/quaza/solutions/qpalx/elearning/web/lms/adaptivelearning/StudentAdaptiveLearningController.java
+++ qpalx-elearning-web/src/main/java/com/quaza/solutions/qpalx/elearning/web/lms/adaptivelearning/StudentAdaptiveLearningController.java
@@ -3,7 +3,6 @@ package com.quaza.solutions.qpalx.elearning.web.lms.adaptivelearning;
 import com.quaza.solutions.qpalx.elearning.domain.lms.curriculum.ELearningCourse;
 import com.quaza.solutions.qpalx.elearning.domain.lms.curriculum.QPalXELesson;
 import com.quaza.solutions.qpalx.elearning.domain.lms.curriculum.QPalXEMicroLesson;
-import com.quaza.solutions.qpalx.elearning.domain.qpalxuser.QPalXUser;
 import com.quaza.solutions.qpalx.elearning.domain.tutoriallevel.TutorialLevelCalendar;
 import com.quaza.solutions.qpalx.elearning.service.lms.curriculum.IELearningCourseService;
 import com.quaza.solutions.qpalx.elearning.service.lms.curriculum.IQPalXELessonService;
@@ -14,7 +13,6 @@ import com.quaza.solutions.qpalx.elearning.web.display.attributes.enums.Curricul
 import com.quaza.solutions.qpalx.elearning.web.lms.curriculum.enums.LessonsAdminAttributesE;
 import com.quaza.solutions.qpalx.elearning.web.service.panel.IQPalXUserInfoPanelService;
 import com.quaza.solutions.qpalx.elearning.web.service.panel.ITutorialLevelCalendarPanelService;
-import com.quaza.solutions.qpalx.elearning.web.service.user.IQPalXUserWebService;
 import com.quaza.solutions.qpalx.elearning.web.student.curricula.StudentCurriculaController;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,7 +24,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
-import java.util.Optional;
 
 /**
  * @author manyce400
@@ -35,10 +32,6 @@ import java.util.Optional;
 public class StudentAdaptiveLearningController {
 
 
-
-    @Autowired
-    @Qualifier("quaza.solutions.qpalx.elearning.web.QPalXUserWebService")
-    private IQPalXUserWebService iqPalXUserWebService;
 
     @Autowired
     @Qualifier("quaza.solutions.qpalx.elearning.service.DefaultELearningCourseService")
@@ -71,13 +64,11 @@ public class StudentAdaptiveLearningController {
     public String viewAdaptiveLessons(final Model model, @RequestParam("eLearningCourseID") String eLearningCourseID, @RequestParam("tutorialLevelID") String tutorialLevelID) {
         LOGGER.info("Finding and displaying all lessons for courseID: {} and tutorialLevelID: {}", eLearningCourseID, tutorialLevelID);
 
-        Optional<QPalXUser> optionalUser = iqPalXUserWebService.getLoggedInQPalXUser();
-
         Long cId = NumberUtils.toLong(eLearningCourseID);
         ELearningCourse eLearningCourse = ieLearningCourseService.findByCourseID(cId);
         model.addAttribute(CurriculumDisplayAttributeE.SelectedELearningCourse.toString(), eLearningCourse);
 
-        // Find the current default TutorialLevelCalendar based on the current month
+        // Find the current default TutorialLevelCalendar based on the selected value
         Long tId = NumberUtils.toLong(tutorialLevelID);
         TutorialLevelCalendar selectedTutorialLevelCalendar = iTutorialLevelCalendarService.findByID(tId);
         List<QPalXELesson> qPalXELessons = iqPalXELessonService.findQPalXELessonByCalendarAndCourse(selectedTutorialLevelCalendar, eLearningCourse);
@@ -92,11 +83,8 @@ public class StudentAdaptiveLearningController {
     }
 
     @RequestMapping(value = "/view-micro-lessons", method = RequestMethod.GET)
-    public String viewAdaptiveMicroLessons(final Model model, @RequestParam("eLessonID") String eLessonID,
-                                           @RequestParam("user_name") String userName, @RequestParam("user_email") String userEmail, @RequestParam("user_id") String userID) {
+    public String viewAdaptiveMicroLessons(final Model model, @RequestParam("eLessonID") String eLessonID, @RequestParam("tutorialLevelID") String tutorialLevelID) {
         LOGGER.info("Finding and displaying all micro lessons foe eLessonID: {}", eLessonID);
-
-        Optional<QPalXUser> optionalUser = iqPalXUserWebService.getLoggedInQPalXUser();
 
         Long id = NumberUtils.toLong(eLessonID);
         QPalXELesson qPalXELesson = iqPalXELessonService.findQPalXELessonByID(id);
@@ -110,12 +98,13 @@ public class StudentAdaptiveLearningController {
         List<QPalXEMicroLesson> qPalXEMicroLessons = iqPalXEMicroLessonService.findQPalXEMicroLessons(qPalXELesson);
         model.addAttribute(LessonsAdminAttributesE.QPalXEMicroLessons.toString(), qPalXEMicroLessons);
 
-        Optional<TutorialLevelCalendar> selectedTutorialLevelCalendar = iTutorialLevelCalendarService.findCurrentCalendarByTutorialLevel(optionalUser.get());
-        // Add all attributes required for Student tutorial level calendar panel.  By Default we load only first term if no calendar is specified
-        iTutorialLevelCalendarPanelService.addCalendarPanelInfo(model, selectedTutorialLevelCalendar.get());
+        // Find the current default TutorialLevelCalendar based on the selected value
+        Long tId = NumberUtils.toLong(tutorialLevelID);
+        iTutorialLevelCalendarPanelService.addCalendarPanelInfo(model, tId);
 
         // Add all attributes required for User information panel
         qPalXUserInfoPanelService.addUserInfoAttributes(model);
+        LOGGER.info("Returning micro lessons display page ==> micro-lesson-display");
         return ContentRootE.Student_Adaptive_Learning.getContentRootPagePath("micro-lesson-display");
     }
 }
