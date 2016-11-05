f049d8c928a697b0e6f86ae3fb628dfa72f285ce
manyce400 <fallon12>
2016-11-04 23:37:06 -0400

    Version 1.0 functionality:  Resolved issues with tracking QuestionBankItem progress

--- qpalx-elearning-domain/src/main/java/com/quaza/solutions/qpalx/elearning/domain/lms/adaptivelearning/progress/repository/IQuestionBankProgressRepository.java
+++ qpalx-elearning-domain/src/main/java/com/quaza/solutions/qpalx/elearning/domain/lms/adaptivelearning/progress/repository/IQuestionBankProgressRepository.java
@@ -1,6 +1,7 @@
 package com.quaza.solutions.qpalx.elearning.domain.lms.adaptivelearning.progress.repository;
 
 import com.quaza.solutions.qpalx.elearning.domain.lms.adaptivelearning.progress.QuestionBankProgress;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 /**
@@ -8,5 +9,11 @@ import org.springframework.data.repository.CrudRepository;
  */
 public interface IQuestionBankProgressRepository extends CrudRepository<QuestionBankProgress, Long> {
 
+
+    @Query("Select               questionBankProgress From QuestionBankProgress questionBankProgress "+
+            "Where               questionBankProgress.questionBankItemID =?1 "+
+            "And                 questionBankProgress.qPalxUserID =?2"
+    )
+    public QuestionBankProgress findByQuestionBankItemAndUser(Long questionBankItemID, Long qPalXUserID);
 
 }
