50563373cce272fa6b04f54f50ec7ee5ce8e642d
manyce400 <fallon12>
2016-11-20 21:20:20 -0500

    Version 1.0 functionality:  Adding explainer to homepage.

--- qpalx-elearning-web/src/main/java/com/quaza/solutions/qpalx/elearning/web/signup/student/StudentRegistrationController.java
+++ qpalx-elearning-web/src/main/java/com/quaza/solutions/qpalx/elearning/web/signup/student/StudentRegistrationController.java
@@ -70,7 +70,7 @@ public class StudentRegistrationController {
     @RequestMapping(value = "/select-signup-payment", method = RequestMethod.POST)
     public String accessRegistrationPayment(Model model, @ModelAttribute("QPalXWebUserVO") QPalXWebUserVO qPalXWebUserVO) {
         LOGGER.info("Student signup payment requested with qPalXWebUserVO: {}", qPalXWebUserVO);
-        return ContentRootE.Student_Signup.getContentRootPagePath("payment");
+        return ContentRootE.Student_Signup.getContentRootPagePath("payment-selection");
     }
 
     @RequestMapping(value = "/customize-proficiency-ranking", method = RequestMethod.POST)
@@ -103,7 +103,7 @@ public class StudentRegistrationController {
                 qPalXWebUserVO.setIncorrectValueCounter(holder);
                 System.out.println("Failed Attempts: " + qPalXWebUserVO.getIncorrectValueCounter());
                 model.addAttribute("QPalXWebUserVO", qPalXWebUserVO);
-                return ContentRootE.Student_Signup.getContentRootPagePath("payment");
+                return ContentRootE.Student_Signup.getContentRootPagePath("payment-selection");
             }
         }
     }
