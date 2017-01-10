9e212b30d24cc49f11eedb8e0e7de82e0b8f680d
manyce400 <fallon12>
2017-01-09 19:13:43 -0500

    Version 1.0 functionality:  Increasing size of Quiz Question feedback.  Adding dynamic loading of Quiz banner icons

---
+++ qpalx-elearning-service/src/main/java/com/quaza/solutions/qpalx/elearning/service/councurrent/ApplicationConcurrencyConfig.java
@@ -0,0 +1,24 @@
+package com.quaza.solutions.qpalx.elearning.service.councurrent;
+
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.Executors;
+
+/**
+ * @author manyce400
+ */
+@Configuration
+public class ApplicationConcurrencyConfig {
+
+
+    @Bean(name = "quaza.solutions.qpalx.elearning.service.StatisticsExecutorService")
+    public ListeningExecutorService configureApplicationExecutorService() {
+        ListeningExecutorService executorService = MoreExecutors.listeningDecorator(
+                Executors.newFixedThreadPool(5, ApplicationThreadFactoryBuilder.buildThreadFactory("STATISTICS-QPalX-ThreadPool-%d"))
+        );
+        return executorService;
+    }
+}
